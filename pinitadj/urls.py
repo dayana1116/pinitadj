6df104ca2d0510add9db9bdbdc8cc7051dbadcab
dayana ornelas <dayanaOHdz@outlook.coim>
2024-01-07 20:51:48 -0600

    le agregue a la app de login el archivo urls para configurar

--- pinitadj/urls.py
+++ pinitadj/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',include('Index.urls')),
     path('moderador/',include('moderador.urls')),
+    path('login/',include('login.urls')),
     
 ]
