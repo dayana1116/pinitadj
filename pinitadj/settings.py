ee1c793b246608a2cbbb7859e1db25c62faaf6af
dayana ornelas <dayanaOHdz@outlook.coim>
2024-01-08 18:27:41 -0600

    agregue la vista y urls de la app de single ademas de que agregue a app en settings.py

--- pinitadj/settings.py
+++ pinitadj/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'Fundacion',
     'registro',
     'contacto',
+    'single',
 ]
 
 MIDDLEWARE = [
--- single/views.py
+++ single/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def single(request):
+    return render(request, 'single.html')
