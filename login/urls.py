a714169c9b9aad6cfb209ddb26b67a288b800868
dayana ornelas <dayanaOHdz@outlook.coim>
2024-01-11 14:43:28 -0600

    agregando funciones de django para el login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+
 #views
-""" from . import views
+from . import views
 urlpatterns =[
-    path('', views.login, name='login')
-] """
+    path('', views.login, name='login'),
+    path('logout',views.logout_user, name='logout')
+] 
--- login/views.py
+++ login/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-""" def login(request):
-    return render(request,'formlogin.html') """
+def login(request):
+    return render(request,'formlogin.html') 
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Autenticar al usuario con las credenciales proporcionadas
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # El usuario es autenticado, iniciar sesión
+            login(request, user)
+            return redirect('controlBlog')
+        else:
+            # El usuario no es autenticado, mostrar un mensaje de error
+            messages.error(request, 'El nombre de usuario o contrseña no son correctos.')
+            return redirect('login')
+
+    else:
+        return render(request, 'formlogin.html')
+def logout_user(request):
+    # Cerrar sesión del usuario
+    logout(request)
+    return redirect('login')
