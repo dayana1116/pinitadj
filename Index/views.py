43f42517efee995601408e82dd2b4635de713f48
dayana ornelas <dayanaOHdz@outlook.coim>
2024-06-18 14:43:47 -0600

    cammbios guardados 18/06/2024-2:43

--- Index/views.py
+++ Index/views.py
@@ -9,11 +9,12 @@ def index(request):
 # esta funcion sirve para mostrar los post la parte del blogpost en la parte de la interfaz para el usuario
 def vista_post(request, idpost):
     post = Post.objects.get(id=idpost)  # Obtener el post específico según su id
-    return render(request, 'index.html', {'posts': [post]})
+    
+    return render(request, 'index.html', {'posts': post})
 
 # con esta funcion se visualizara solo un id del post
 def single_post(request, post_id):
     post = Post.objects.get(pk=post_id)
-    return render(request, 'single.html', {'post': post})
+    return render(request, 'single.html', {'post': [post]})
     
 
